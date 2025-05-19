a152ff3411d945afcddc0169d6d807a7fb5ae8c0
LudmiAbril <ludmila.pereyra543@gmail.com>
2025-05-19 01:01:05 -0300

    coment

--- auth/src/main.tsx
+++ auth/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// remover extension, remover vite, arreglar lo de la importacion de interfaces
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
