c8c1a34219a8a4e2376c71071606ab6d1b697c1c
Celena <celutaumos@gmail.com>
2025-05-20 19:48:47 -0300

    tailwinf ocnfig

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -10,9 +10,15 @@ export default function Navbar() {
   const [mostrarModal, setMostrarModal] = useState(false);
   const [formulario, setFormulario] = useState<"login" | "registro">("login");
 
+
   return (
     <>
-      <header className="fixed top-0 left-0 z-[9999] w-full bg-transparent text-white px-10 pt-2 flex justify-between items-center">
+      <header
+        className={`fixed top-0 left-0 z-[9999] w-full bg-transparent px-10 pt-2 flex justify-between items-center transition-colors duration-200 ${
+     "text-white"
+        }`}
+
+      >
         <img src="/logo/logo.svg" alt="Logo" className="w-[80px]" />
 
         <div className="flex items-center gap-[50px]">
@@ -23,12 +29,18 @@ export default function Navbar() {
             <Link href="/proveedores">Proveedores</Link>
           </nav>
 
-          <img  src="/svgGeneral/user.svg" alt="Perfil" className="w-[30px] h-[30px] object-contain cursor-pointer"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-[30px] h-[30px] cursor-pointer transition-colors duration-200"
             onClick={() => {
               setFormulario("login");
               setMostrarModal(true);
             }}
-          />
+            fill="currentColor"
+          >
+            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+          </svg>
         </div>
       </header>
 
@@ -44,8 +56,6 @@ export default function Navbar() {
           )}
         </AuthModalWrapper>
       )}
-
-
     </>
   );
 }
