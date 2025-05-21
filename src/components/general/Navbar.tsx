6ed0b4e0023dce34bdaebabd9d0b3dab579844f6
LudmiAbril <ludmila.pereyra543@gmail.com>
2025-05-21 00:14:46 -0300

    navbar color

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -5,22 +5,29 @@ import Link from "next/link";
 import AuthModalWrapper from "@/components/login/AuthModalWrapper";
 import InicioSesionForm from "@/components/login/InicioSesionForm";
 import RegistroForm from "@/components/login/registro";
+import { usePathname } from "next/navigation";
+import logo from "@/public/logo/logo.svg";
+import logoVioleta from "@/public/logo/logo-violeta.svg";
+
 
 export default function Navbar() {
   const [mostrarModal, setMostrarModal] = useState(false);
   const [formulario, setFormulario] = useState<"login" | "registro">("login");
-
+  const pathname = usePathname()
+  const isHome = pathname === '/'
 
   return (
     <>
       <header
-        className={`fixed top-0 left-0 z-[9999] w-full bg-transparent px-10 pt-2 flex justify-between items-center transition-colors duration-200 ${
-     "text-white"
-        }`}
+        className={`fixed top-0 left-0 z-[9999] w-full bg-transparent px-10 pt-2 flex justify-between items-center transition-colors duration-200 ${isHome ? "text-white" : "text-[var(--violeta)]"
+          }`}
 
       >
-        <img src="/logo/logo.svg" alt="Logo" className="w-[80px]" />
-
+        <img
+          src={isHome ? "/logo/logo-blanco.svg" : "/logo/logo-violeta.svg"}
+          alt="Logo"
+          className="w-[62px]"
+        />
         <div className="flex items-center gap-[50px]">
           <nav className="flex items-center gap-[50px]">
             <Link href="/">Home</Link>
@@ -28,7 +35,6 @@ export default function Navbar() {
             <Link href="/descubrir">Descubrir</Link>
             <Link href="/proveedores">Proveedores</Link>
           </nav>
-
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -42,7 +48,7 @@ export default function Navbar() {
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
           </svg>
         </div>
-      </header>
+      </header >
 
       {mostrarModal && (
         <AuthModalWrapper
@@ -55,7 +61,8 @@ export default function Navbar() {
             <RegistroForm cambiarFormulario={() => setFormulario("login")} />
           )}
         </AuthModalWrapper>
-      )}
+      )
+      }
     </>
   );
 }
