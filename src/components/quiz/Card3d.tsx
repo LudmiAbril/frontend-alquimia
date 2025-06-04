26adcdf957332a7875940e33b63ab7aad69e2c98
Celena <celutaumos@gmail.com>
2025-06-03 22:57:13 -0300

    quiz

--- src/components/quiz/Card3d.tsx
+++ src/components/quiz/Card3d.tsx
@@ -6,12 +6,12 @@ interface Card3DProps {
   backgroundSrc: string
   characterSrc: string
   alt: string
-  title: string // nuevo
+  title: string
 }
 
 export default function Card3D({ backgroundSrc, characterSrc, alt, title }: Card3DProps) {
   return (
-    <div className="card relative w-[200px] h-[300px] flex justify-center items-end perspective-[2500px] group">
+    <div className="card relative w-[200px] h-[300px] flex justify-center items-end perspective-[2500px] group overflow-hidden rounded-3xl">
       <div className="wrapper absolute w-full h-full rounded-3xl overflow-hidden transition-all duration-500 group-hover:translate-y-[-5%] group-hover:rotate-x-[25deg] group-hover:shadow-2xl">
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[rgba(12,13,19,0.9)] to-transparent z-0"></div>
         <Image
@@ -31,12 +31,11 @@ export default function Card3D({ backgroundSrc, characterSrc, alt, title }: Card
       />
 
       {/* Texto animado */}
- <div className="absolute bottom-2 w-full text-center z-20 opacity-0 group-hover:opacity-100 transition-all duration-500 translate-y-2 group-hover:translate-y-0">
-  <span className="text-white text-base font-bold tracking-wide font-[var(--font-principal)] glow bg-[rgba(0,0,0,0.3)] px-2 py-1 rounded-md">
-    {title}
-  </span>
-</div>
-
+      <div className="absolute bottom-6 w-full text-center z-20 opacity-0 group-hover:opacity-100 transition-all duration-500 translate-y-2 group-hover:translate-y-0">
+        <span className="text-white text-sm font-bold tracking-wide font-[var(--font-principal)] glow bg-[rgba(0,0,0,0.4)] px-3 py-1 rounded-md shadow-md">
+          {title}
+        </span>
+      </div>
     </div>
   )
 }
