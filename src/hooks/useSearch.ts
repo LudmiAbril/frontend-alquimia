14b22584dbb62f782baab2fdadd51251c687dc4f
Axel <axeelleguero@gmail.com>
2025-06-07 12:00:19 -0300

    No recuerdo algo del buscador de notas

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react"
 import { PerfumeNotesResult } from "@/components/utils/typing"
 export function useSearch() {
   const [query, setQuery] = useState("")
-  const [results, setResults] = useState<PerfumeNotesResult | null>(null)
+  const [result, setResult] = useState<PerfumeNotesResult | null>(null)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      if (!query.trim()) return setResults(null)
+      if (!query.trim()) return setResult(null)
 
       setIsLoading(true)
 
@@ -15,12 +15,19 @@ export function useSearch() {
       //fetch(`/api/search?q=${query}`)
       fetch(`http://localhost:8000/api/perfume?q=${encodeURIComponent(query)}`)
         .then((res) => res.json())
-        .then((data) => setResults(data))
+        .then((data) => {
+          if (data.notes && data.notes.length > 0) {
+            setResult(data)
+          } else {
+            setResult(null)
+          }
+        })
+        .catch(() => setResult(null))
         .finally(() => setIsLoading(false))
     }, 500)
 
     return () => clearTimeout(timeout)
   }, [query])
 
-  return { query, setQuery, results, isLoading }
+  return { query, setQuery, result, isLoading }
 }
