acbdf5c9ccc70f8eda81a874cc02c559b80984b2
Celena <celutaumos@gmail.com>
2025-06-08 17:38:23 -0300

    tooltip

--- src/components/quiz/DynamicQuestion.tsx
+++ src/components/quiz/DynamicQuestion.tsx
@@ -5,6 +5,7 @@ import OptionList from "./visual/OptionList"
 import OptionCard from "./visual/OptionCard"
 import OptionBubble from "./visual/OptionBubble"
 import OptionTwo from "./visual/OptionTwo"
+import TooltipQuestion from "./TooltipQuestion"
 
 
 
@@ -20,23 +21,29 @@ const overrideMap: Record<number, string> = {
 9:"grid",
 10:"cards"
 }
-
 export default function DynamicQuestion({ question, selectedOption, onSelect }: PropsDynamic) {
   const visualType = overrideMap[Number(question.Id)] || question.VisualType
-console.log("Pregunta ID:", question.Id)
-console.log("Componente a renderizar:", visualType)
-  switch (visualType) {
-  case "grid":
-    return <OptionGrid question={question} selectedOption={selectedOption} onSelect={onSelect} />
-  case "bubbles":
-    return <OptionBubble question={question} selectedOption={selectedOption} onSelect={onSelect} />
- 
-  case "list":
-    return <OptionList question={question} selectedOption={selectedOption} onSelect={onSelect} />
-  case "two":
-    return <OptionTwo question={question} selectedOption={selectedOption} onSelect={onSelect} />
-  default:
-    return <OptionCard question={question} selectedOption={selectedOption} onSelect={onSelect} />
+  const questionId = Number(question.Id)
+
+  return (
+    <>
+      <TooltipQuestion id={questionId} />
+      {(() => {
+        switch (visualType) {
+          case "grid":
+            return <OptionGrid question={question} selectedOption={selectedOption} onSelect={onSelect} />
+          case "bubbles":
+            return <OptionBubble question={question} selectedOption={selectedOption} onSelect={onSelect} />
+          case "list":
+            return <OptionList question={question} selectedOption={selectedOption} onSelect={onSelect} />
+          case "two":
+            return <OptionTwo question={question} selectedOption={selectedOption} onSelect={onSelect} />
+          default:
+            return <OptionCard question={question} selectedOption={selectedOption} onSelect={onSelect} />
+        }
+      })()}
+    </>
+  )
 }
 
-}
+
