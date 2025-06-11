a11e78ff44ce979f8725d2a9624a46dd4547c5ff
PonceIvan <82486446+PonceIvan@users.noreply.github.com>
2025-06-11 07:34:55 -0300

    Update mp.ts

--- src/services/mp.ts
+++ src/services/mp.ts
@@ -1,3 +1,3 @@
 export function getMockCheckoutUrl(): string {
-  return "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=412861077-ae500539-9a13-4a4f-a1cc-13625b23cfdc";
+  return "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=412861077-d2df5906-cab8-449d-af45-d2cf68b1dde3";
 }
